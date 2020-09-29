e44c78779fb00e017dffa0e0ebb94a3adf80b4e6
JC Nolan <jc@jcnolan.com>
2020-09-28 20:15:33 -0700

    Initial Commit

---
+++ ConsoleViewSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ConsoleViewSample
+//
+//  Created by JC Nolan on 9/28/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
